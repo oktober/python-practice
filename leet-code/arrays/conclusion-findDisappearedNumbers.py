03c48fcb47be7ec339f4dd80b757b8b196b579d7
oktober <5934988+oktober@users.noreply.github.com>
2022-04-26 08:12:54 -0700

    Update conclusion-findDisappearedNumbers.py
    Added a pythonic way to solve the problem and one using no additional arrays

--- leet-code/arrays/conclusion-findDisappearedNumbers.py
+++ leet-code/arrays/conclusion-findDisappearedNumbers.py
@@ -19,8 +19,8 @@ def test_findDisappearedNumbers2():
 
 	assert got == want
 
-# this code exceeds the time limit
 def findDisappearedNumbers(nums: List[int]):
+	# my original solution
 	# create a unique set of numbers
 	unique = set(nums)
 	# create a new list to store the results
@@ -34,13 +34,41 @@ def findDisappearedNumbers(nums: List[int]):
 			disappeared.append(digit)
 
 	return disappeared
+	"""
+	LeetCode:
+		Runtime: 411ms
+		Memory Usage: 24.7MB
+	"""
+
+	# # pythonic way
+	# unique = set(nums)
+	# return [digit for digit in range(1, len(nums)+1) if digit not in unique]
+	"""
+	LeetCode:
+		Runtime: 413ms
+		Memory Usage: 24.4MB
+	"""
+
+	# # pythonic way using only original array
+	# for index in range(len(nums)):
+	# 	# get the absolute value for this index
+	# 	absDigit = abs(nums[index])
+	# 	# if the value at the index before this absolute value is positive
+	# 		# since we're starting at 1, not 0 for the values we're looking for
+	# 		# e.g. the 4th spot would be found at nums[4-1] -> nums[3]
+	# 	if nums[absDigit-1] > 0:
+	# 		# set it to a negative number - mark it as "found"
+	# 		nums[absDigit-1] = nums[absDigit-1] * -1
+
+	# # return the index+1 (to get values from 1 to n) if this value was not "found"
+	# 	# i.e. if it was not marked negative
+	# return [index+1 for index in range(len(nums)) if nums[index] > 0]
+	"""
+	LeetCode:
+		Runtime: 705ms
+		Memory Usage: 22.2MB
+	"""
 
 if __name__ == "__main__":
 	print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
 	print(findDisappearedNumbers([1,1]))
-
-"""
-LeetCode:
-	Runtime: 411ms
-	Memory Usage: 24.7MB
-"""
